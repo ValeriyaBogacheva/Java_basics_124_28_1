d6b5121b91909233240db6beae3eff6a1ede39d6
Lenovo <Valeriya_bogacheva@mail.ru>
2023-03-09 21:58:42 +0300

    деление int

--- src/src/itmo/java/basics/lasson2/Calculator.java
+++ src/src/itmo/java/basics/lasson2/Calculator.java
@@ -16,6 +16,9 @@ public class Calculator {
     public double division(long a1, long a2){
         return (double) a1 / a2;
     }
+    public double division(int a1, int a2){
+        return (double) a1 / a2;
+    }
     public double multi(double a1, double a2){
         return a1 * a2;
     }
